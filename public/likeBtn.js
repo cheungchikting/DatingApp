31a985bf2ddc0709cd0d62f9dde8ab995e787783
Caspartai <taikalungcc@gmail.com>
2021-06-12 22:51:57 +0800

    css modify

--- public/likeBtn.js
+++ public/likeBtn.js
@@ -8,6 +8,16 @@ $(function () {
         .done((data) => {
           if (JSON.parse(data).checkMatch.indexOf(data.user_id) > -1) {
             console.log('this is a match!')
+            $('.navbar_container').parent().append(
+              `<div class="wrapper active">
+
+              <div class="container-btn match-finish active">
+                <i class="fas fa-times close"></i>
+                <p class="text">You have like the person!</p>
+                <a href="" class="btn-match">Chat Now!</a>
+              </div>
+          
+             </div>`)
 
           }
           window.location.href = "/findmatches"
@@ -24,8 +34,16 @@ $(function () {
           console.log(JSON.parse(data).checkMatch[0])
           if (JSON.parse(data).checkMatch.indexOf(data.user_id) > -1) {
             console.log('this is a match!')
-            
-        
+            $('.navbar_container').parent().append(
+              `<div class="wrapper active">
+
+              <div class="container-btn match-finish active">
+                <i class="fas fa-times close"></i>
+                <p class="text">You have like the person!</p>
+                <a href="" class="btn-match">Chat Now!</a>
+              </div>
+          
+             </div>`)
 
 
           }
